a7ab6dc5c2e36c0bdc1a86e3dc985309a3babc62
Kim Seongsik <josephkim583@gmail.com>
2019-02-21 06:30:35 -0600

    all four endpoints added for existing categories

--- quotes.py
+++ quotes.py
@@ -1,11 +1,31 @@
-import flask
+from flask import Flask, jsonify, request
+import quotesdb
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return "This shit gets quotes"
 
-app.run()
+# Takes no parameters and returns random quote
+@app.route('/api/generalSearch', methods=['GET'])
+def general_search_quote():
+    return quotesdb.read_from_db(None, None) 
+
+# Takes theme parameter and returns random quote with related theme
+@app.route('/api/themeSearch/<theme>', methods = ['GET'])
+def theme_search_quote(theme):
+    return quotesdb.read_from_db(theme, None)
+
+# Takes author parameter and returns random quote with related author
+@app.route('/api/authorSearch/<author>', methods = ['GET'])
+def author_search_quote(author):
+    return quotesdb.read_from_db(None, author)
+
+# Takes theme and author parameters and returns random quote with related theme and author
+@app.route('/api/allCategorySearch/<theme>/<author>', methods = ['GET'])
+def category_search_quote(theme, author):
+    return quotesdb.read_from_db(theme, author)
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- quotesdb.py
+++ quotesdb.py
@@ -1,7 +1,7 @@
 import sqlite3
 import random
 
-conn = sqlite3.connect('quotes.db')
+conn = sqlite3.connect('quotes.db', check_same_thread=False)
 c = conn.cursor()
 
 #Create initial table quotesList
@@ -17,8 +17,10 @@ def data_entry(theme, author, quote):
 #Given category parameters get a random quote related to the provided categories
 def read_from_db(theme, author):
     quotes = []
+    if theme == None and author == None:
+        c.execute('''SELECT quote FROM quotesList''')
 
-    if theme == None or author == None:
+    elif theme == None or author == None:
         c.execute('''SELECT quote FROM quotesList WHERE theme = :theme 
                                                     OR author = :author''',
                                                     {'theme': theme, 'author' : author})
@@ -26,29 +28,29 @@ def read_from_db(theme, author):
         c.execute('''SELECT quote FROM quotesList WHERE theme = :theme 
                                                     AND author = :author''',
                                                     {'theme': theme, 'author' : author})
-
+    
     #Add all quotes with matching categories into quotes list    
     for row in c.fetchall():
         quotes.append(row[0])
 
     if len(quotes) == 0:
-        return None
+        return "No entries are available"
     else:
-        print (quotes[random.randint(0,len(quotes) - 1)])
+        return (quotes[random.randint(0,len(quotes) - 1)])
 
-read_from_db('Life', "Abraham Lincoln")
+# read_from_db(None, None)
 #Hard coded quotesList table for now
 
 # create_table()
-# data_entry("Life", "George Bernard Shaw", "Life isn't about finding yourself. Life is about creating yourself.")
-# data_entry("Life", "Abraham Lincoln", "In the end, it's not the years in your life that count. It's the life in your years.")
+# data_entry("Life", "Shaw", "Life isn't about finding yourself. Life is about creating yourself.")
+# data_entry("Life", "Lincoln", "In the end, it's not the years in your life that count. It's the life in your years.")
 # data_entry("Life", "Socrates", "An unexamined life is not worth living.")
-# data_entry("Life", "Alexander Graham Bell", "When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one that has opened for us.")
-# data_entry("Life", "Abraham Lincoln", "Slavery sucks.")
+# data_entry("Life", "Bell", "When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one that has opened for us.")
+# data_entry("Life", "Lincoln", "Slavery sucks.")
 
-# data_entry("Love", "Mark Jung", "You may hold my hand for a while, but you hold my heart forever.")
-# data_entry("Love", "Mark Jung", "I know I am in love with you because my reality is finally better than my dreams.")
-# data_entry("Love", "Joseph Kim", "Love is great.")
-# data_entry("Love", "Joseph Kim", "I love my princess.")
+# data_entry("Love", "Jung", "You may hold my hand for a while, but you hold my heart forever.")
+# data_entry("Love", "Jung", "I know I am in love with you because my reality is finally better than my dreams.")
+# data_entry("Love", "Kim", "Love is great.")
+# data_entry("Love", "Kim", "I love my princess.")
 
 
