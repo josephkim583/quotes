ab4924588f8954385e7d3284f688a8d7f99e5d07
Kim Seongsik <josephkim583@gmail.com>
2019-03-01 00:25:33 -0600

    get quote test added

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -3,22 +3,36 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 basedir = os.path.abspath(os.path.dirname(__file__))
 import unittest
 import tempfile
+from app import app
+from db import db
+import json
 
 TEST_DB = 'quotes.db'
+
+list_of_quotes = ['Love is great', 'Fuck you', 'Slavery sucks', 'Code as clean as my room',
+                  'SEX', 'Money brings happiness', 'Here he is at 19 getting laid', 'Black people have big dicks']
 
 class BasicTests(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
         app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'quotes.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///quotes.db')
         self.app = app.test_client()
         db.init_app(app)
-        db.drop_all(app=app)
+        # db.drop_all(app=app)
         db.create_all(app=app)
 
     def tearDown(self):
         pass
+
+
+    def test_get_quotes(self):
+        get_quote = self.app.get('/api/allCategorySearch/Life/Lincoln')
+        data_get_quote = json.loads(get_quote.data.decode())
+        self.assertEqual(len(data_get_quote), 1)
+        self.assertEqual(len(data_get_quote['response']), 4)
+        self.assertEqual(data_get_quote['response']['quote'] in list_of_quotes, True)
 
 
 if __name__ == '__main__':
